35176aa842b011dbb8ee2c0dd71f4eb1e31773d3
Ben Jerome <bjerome43@massmutual.com>
2019-04-12 14:51:44 -0400

    added new scroll functions

--- appium/lib/screen-object.rb
+++ appium/lib/screen-object.rb
@@ -1,21 +1,21 @@
-=begin
-***********************************************************************************************************
-SPDX-Copyright: Copyright (c) Capital One Services, LLC
-SPDX-License-Identifier: Apache-2.0
-Copyright 2016 Capital One Services, LLC
+# frozen_string_literal: true
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License. 
-***********************************************************************************************************
-=end
+# ***********************************************************************************************************
+# SPDX-Copyright: Copyright (c) Capital One Services, LLC
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2016 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+# ***********************************************************************************************************
 
 require 'appium_lib'
 require 'screen-object/load_appium'
@@ -28,17 +28,12 @@ require 'screen-object/accessors/element'
 # This module will add instance methods and screen object that you use to define and interact with mobile objects
 
 module ScreenObject
-
   def self.included(cls)
     cls.extend ScreenObject::Accessors
   end
 
   def driver
     ScreenObject::AppElements::Element.new('').driver
-  end
-
-  def swipe(start_x,start_y,end_x,end_y,touch_count,duration)
-    driver.swipe(:start_x => start_x, :start_y => start_y, :end_x => end_x, :end_y => end_y,:touchCount => touch_count,:duration => duration)
   end
 
   def landscape
@@ -48,7 +43,7 @@ module ScreenObject
   def portrait
     driver.driver.rotate :portrait
   end
-  
+
   def back
     driver.back
   end
@@ -56,108 +51,134 @@ module ScreenObject
   def wait_until(timeout = 5, message = nil, &block)
     wait = Selenium::WebDriver::Wait.new(timeout: timeout, message: message)
     wait.until &block
+    sleep 0.2
   end
 
   def wait_step(timeout = 5, message = nil, &block)
     default_wait = driver.default_wait
-    wait = Selenium::WebDriver::Wait.new(:timeout => driver.set_wait(timeout), :message => message)
+    wait = Selenium::WebDriver::Wait.new(timeout: driver.set_wait(timeout), message: message)
     wait.until &block
     driver.set_wait(default_wait)
   end
 
   def enter
-    #pending implementation
+    driver.send_keys(:enter)
   end
 
-  def scroll_down_find(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollStart,:end_x =>0,:end_y =>scrollEnd,:touchCount => 2,:duration => 0)
-        false
-      end
+  def gesture(arg)
+    Appium::TouchAction.new(driver).swipe(start_x: arg[0], start_y: arg[1], end_x: arg[2], end_y: arg[3], duration: arg[4]).perform
+  rescue RuntimeError => e
+    raise("Error during gesture \n Error Details: #{e}")
+  end
+
+  def scroll(direction = :down, duration = 1000)
+    size = driver.window_size
+    x = size.width / 2
+    y = size.height / 2
+    loc = case direction
+          when :up then    [x, y * 0.5, x, (y + (y * 0.5)), duration]
+          when :down then  [x, y, x, y * 0.5, duration]
+          when :left then  [x * 0.6, y, x * 0.3, y, duration]
+          when :right then [x * 0.3, y, x * 0.6, y, duration]
+          else
+            raise('Only upwards and downwards scrolling are supported')
+          end
+    gesture(loc)
+  end
+
+  def scroll_down
+    scroll(:down)
+  end
+
+  def scroll_up
+    scroll(:up)
+  end
+
+  def swipe_left
+    scroll(:left)
+  end
+
+  def swipe_right
+    scroll(:right)
+  end
+
+  def swipe_element(locator, direction = :down, duration = 1000)
+    my_element = driver.find_element(locator.locator.first, locator.locator.last).rect
+    start_x = my_element.x
+    end_x = my_element.x + my_element.width
+    start_y = my_element.y
+    end_y = my_element.y + my_element.height
+    height = my_element.height
+    loc = case direction
+          when :up then [end_x * 0.5, (start_y + (height * 0.2)), end_x * 0.5, (end_y - (height * 0.2)), duration]
+          when :down then  [end_x * 0.5, (end_y - (height * 0.2)), start_x * 0.5, (start_y + (height * 0.3)), duration]
+          when :left then  [end_x * 0.9,  end_y - (height / 2), start_x, end_y - (height / 2), duration]
+          when :right then [end_x * 0.1, end_y - (height / 2), end_x * 0.9, end_y - (height / 2), duration]
+          else
+            raise('Only upwards and downwards scrolling are supported')
+          end
+    gesture(loc)
+  end
+
+  def scroll_down_to_text(text, direction = 'down')
+    if driver.device_is_ios?
+      driver.scroll(direction: direction.to_s, name: text.to_s)
+    else
+      scroll_find(text)
     end
   end
 
-  def scroll_down_click(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          driver.find_element(locator,locator_value).click
-          break
+  def scroll_find(locator={}, click_on = false, direction = :down, num_loop = 15)
+    driver.manage.timeouts.implicit_wait = 1
+    (0..num_loop).each do |i|
+      if locator[:text]
+        if driver.find(locator[:text].to_s).displayed?
+          p locator[:text].to_s
+          if click_on
+            p "clicked"
+            driver.find(locator[:text].to_s).click
+            break
+          else
+            return driver.find(locator[:text].to_s)
+          end
         end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollStart,:end_x =>0,:end_y =>scrollEnd,:touchCount => 2,:duration => 0)
-        false
+      elsif locator[:xpath]
+        if driver.xpath((locator[:xpath]).to_s).displayed?
+          if click_on
+            driver.xpath((locator[:xpath]).to_s).click
+            break
+          else
+            return driver.xpath((locator[:xpath]).to_s)
+          end
+        end
+      else
+      if driver.find_element(":#{locator.locator[0]}", locator.locator[1]).displayed?
+        p "found obj"
+        if click_on
+          p "clicked"
+          driver.find_element(":#{locator.locator[0]}", locator.locator[1]).click
+          break
+        else
+          return driver.find_element(":#{locator.locator[0]}", locator.locator[1])
+        end
       end
+      end
+      raise("#{locator} could not find element") if i == num_loop
+    rescue StandardError
+      scroll(direction)
     end
   end
 
-  def scroll_up_find(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollEnd,:end_x =>0,:end_y =>scrollStart,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
-  end
-
-
-  def scroll_up_click(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          driver.find_element(locator,locator_value).click
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollEnd,:end_x =>0,:end_y =>scrollStart,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
-  end
-
-
-  def drag_and_drop_element(source_locator,source_locator_value,target_locator,target_locator_value)
-    l_draggable = driver.find_element(source_locator,source_locator_value)
-    l_droppable = driver.find_element(target_locator,target_locator_value)
-    obj1= Appium::TouchAction.new
-    obj1.long_press(:x => l_draggable.location.x,:y => l_draggable.location.y).move_to(:x => l_droppable.location.x,:y => l_droppable.location.y).release.perform
+  def drag_and_drop_element(source_locator, source_locator_value, target_locator, target_locator_value)
+    l_draggable = driver.find_element(source_locator, source_locator_value)
+    l_droppable = driver.find_element(target_locator, target_locator_value)
+    obj1 = Appium::TouchAction.new
+    obj1.long_press(x: l_draggable.location.x, y: l_draggable.location.y).move_to(x: l_droppable.location.x, y: l_droppable.location.y).release.perform
   end
 
   def keyboard_hide
-    begin
     driver.hide_keyboard
-    rescue
-      false
-    end
+  rescue StandardError
+    false
   end
-  
 end
