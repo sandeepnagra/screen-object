e5838bbb3dd848ceaeb9e7ea4e6958c6bcc6e55e
Sandeep Singh <ssingh31@massmutual.com>
2019-05-13 10:06:38 -0400

    fixing require

--- appium/lib/screen-object/accessors/element.rb
+++ appium/lib/screen-object/accessors/element.rb
@@ -13,7 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License. 
 ***********************************************************************************************************
 =end
-require_relative '../../../../appium/lib/screen-object'
+
+require 'screen-object'
+
 module ScreenObject
   module AppElements
     class Element
